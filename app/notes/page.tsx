f5864f35e8b8db0bd7d5602d70c401c7a6f4503a
Your Name <your-email@example.com>
2025-11-20 20:36:14 +0300

    Added notes page and supabase connection

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,4 +1,4 @@
-import { createClient } from '@/utils/supabase/server' // Note: check your path, it might be @/lib/supabase/server depending on your starter
+import { createClient } from '@/lib/supabase/server' // Note: check your path, it might be @/lib/supabase/server depending on your starter
 
 export default async function Page() {
   const supabase = await createClient()
