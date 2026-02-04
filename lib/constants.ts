1eda5bc8a38ac708c293819a3e8207a989d97ceb
Natnael <natnaeltekle236@gmail.com>
2026-02-05 01:56:40 +0300

    fix: export DEFAULT_SERVICE_IMAGE from constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -7,3 +7,5 @@ export const CONFIG = {
     DATE_FORMAT: 'MMM dd, yyyy',
     TIME_FORMAT: 'hh:mm a',
 };
+
+export const DEFAULT_SERVICE_IMAGE = 'https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=80';
